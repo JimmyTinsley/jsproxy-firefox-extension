63cf6f0fa7c4138eb4853a7119ac430a3aab06bd
JimmyTinsley <liujie333333@gmail.com>
2019-07-09 17:31:56 +0800

    Separate 'open or search with jsproxy' into two options in the context menu

--- background.js
+++ background.js
@@ -32,29 +32,45 @@ Create the context menu items.
 browser.menus.create({
   id: "open-with-proxy",
   title: browser.i18n.getMessage("menuItemOpenWithProxy"),
-  contexts: ["link", "selection"]
+  contexts: ["link"]
+}, onCreated);
+
+browser.menus.create({
+  id: "search-with-proxy",
+  title: browser.i18n.getMessage("menuItemSearchWithProxy"),
+  contexts: ["selection"]
 }, onCreated);
 
 /*
-Attach prefix obtained from preference before the url got from link or selectionText,
-and then open the combined url in a new browser tab.
-If selected text is not a url, jsproxy will automatically google it. :D
+This function obtains jsproxy sandbox url from preferences for further use.
+*/
+async function getJsproxyPrefix() {
+  var gotItem = await browser.storage.sync.get('jsproxy_sandbox_url');
+  var jsproxy_sandbox_url = gotItem.jsproxy_sandbox_url;
+  if (jsproxy_sandbox_url.slice(-1) !== "/") {
+    jsproxy_sandbox_url = jsproxy_sandbox_url + "/";
+  }
+  return jsproxy_sandbox_url;
+}
+
+/*
+Open the clicked link with jsproxy by attaching a prefix before the link's url.
 */
 async function openWithProxy(info) {
-  var gotItem = await browser.storage.sync.get('jsproxy_sandbox_url');
-  var jsproxy_prefix = gotItem.jsproxy_sandbox_url; 
-  if (jsproxy_prefix.slice(-1) !== "/") {
-    jsproxy_prefix = jsproxy_prefix + "/";
-  }
-  console.log(jsproxy_prefix);
-  var current_url;
-  if (info.selectionText !== undefined) {
-    current_url = info.selectionText;
-  } else {
-    current_url = info.linkUrl;
-  }
+  var jsproxy_prefix = await getJsproxyPrefix();
   var creating = browser.tabs.create({
-    url: jsproxy_prefix + "-----" + current_url
+    url: jsproxy_prefix + "-----" + info.linkUrl
+  });
+  creating.then(onCreated, onError);
+}
+
+/*
+Search the selected keyword with jsproxy-Google by attaching a prefix before selected text.
+*/
+async function searchWithProxy(info) {
+  var jsproxy_prefix = await getJsproxyPrefix();
+  var creating = browser.tabs.create({
+    url: jsproxy_prefix + "-----" + info.selectionText
   });
   creating.then(onCreated, onError);
 }
@@ -67,6 +83,9 @@ browser.menus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case "open-with-proxy":
       openWithProxy(info);
+      break;
+    case "search-with-proxy":
+      searchWithProxy(info);
       break;
   }
 });
